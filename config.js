c30dc31c836210e158c10f195420f2ce72622f06
JIU <jorge.inostroza.u@gmail.com>
2017-03-02 16:19:17 -0400

    Se agrega middleware y config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost:27017/shop'
+  db: 'mongodb://localhost:27017/shop',
+  SECRET_TOKEN: 'miclavedetokens'
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const api = express.Router()
 const productCtrl = require('../controllers/product')
+const auth = require('../middlewares/auth')
 
 api.get('/product', productCtrl.getProducts)
 api.get('/product/:productId', productCtrl.getProduct)
@@ -11,6 +12,9 @@ api.put('/product/:productId',productCtrl.updateProduct)
 api.delete('/product/:productId',productCtrl.deleteProduct)
 api.get('/hola/:name',(req, res)=>{
 	res.send({message:`Hola ${req.params.name}`})
+})
+api.get('/private', auth.isAuth, function(req,res){
+	return res.status(200).send({message: 'Tienes acceso'})
 })
 
 module.exports = api
